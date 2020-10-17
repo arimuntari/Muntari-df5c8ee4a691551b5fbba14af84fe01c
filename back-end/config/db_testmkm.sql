025f9fd24875b55cfae62737bece0aa4345bdcdf
ari muntari <ari.muntari1@gmail.com>
2020-10-17 12:55:13 +0700

    done all

---
+++ back-end/config/db_testmkm.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Ultimate v9.20 
+MySQL - 5.5.5-10.4.6-MariaDB : Database - db_testmkm
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_testmkm` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_testmkm`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `status` char(1) DEFAULT '0',
+  `user_agent` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`last_login`,`status`,`user_agent`) values (1,'Muntari','123456',NULL,'0',NULL),(4,'ari','123','2020-10-17 12:53:47','1','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'),(5,'ari1','1234',NULL,'0',NULL),(6,'ida','123',NULL,'0',NULL),(8,'ida2','123','2020-10-17 12:34:35','1',''),(9,'sadasd','123',NULL,'0',NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
